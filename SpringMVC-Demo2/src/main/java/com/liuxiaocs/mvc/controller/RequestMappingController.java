217c3c7174e97d265bf96300eaba845ce350f850
liuxiao <liuxiao2103@qq.com>
2021-09-04 00:48:44 +0800

    测试GetMapping

--- SpringMVC-Demo2/src/main/java/com/liuxiaocs/mvc/controller/RequestMappingController.java
+++ SpringMVC-Demo2/src/main/java/com/liuxiaocs/mvc/controller/RequestMappingController.java
@@ -1,14 +1,24 @@
 package com.liuxiaocs.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 // @RequestMapping("hello")
 public class RequestMappingController {
 
-    @RequestMapping(value = {"/testRequestMapping", "test"})
+    @RequestMapping(
+            value = {"/testRequestMapping", "test"},
+            method = {RequestMethod.GET}
+    )
     public String success() {
+        return "success";
+    }
+
+    @GetMapping("/testGetMapping")
+    public String testGetMapping() {
         return "success";
     }
 }
