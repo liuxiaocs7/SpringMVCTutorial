b468413a33e8128c64a752d3023173ce7f320ab5
liuxiao <liuxiao2103@qq.com>
2021-09-04 00:31:23 +0800

    测试RequestMapping的value属性

---
+++ SpringMVC-Demo2/src/main/java/com/liuxiaocs/mvc/controller/RequestMappingController.java
@@ -0,0 +1,14 @@
+package com.liuxiaocs.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+// @RequestMapping("hello")
+public class RequestMappingController {
+
+    @RequestMapping(value = {"/testRequestMapping", "test"})
+    public String success() {
+        return "success";
+    }
+}
